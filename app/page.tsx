a59bbcb3ad57eded3c9c6a89d7b57c3c1707859b
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-26 13:14:57 +0530

    Disable CanarayToken in page.tsx
    Comment out the tracking pixel image in page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ export default function BirthdayTributePage() {
       <BirthdaySlideshow />
 
       {/* TRACKING PIXEL STARTS HERE */}
-      <img 
+      {/*<img 
         src="http://canarytokens.com/about/traffic/ow85mcryjyoqzrycogm1rb87v/index.html" 
         alt=""
         style={{ display: 'none', width: '0', height: '0' }}
         aria-hidden="true"
-      />
+      />*/}
       {/* TRACKING PIXEL ENDS HERE */}
     </main>
   )
