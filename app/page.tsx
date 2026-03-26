dbf48f574f1551c98827940b11a7394b7fceafd9
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-26 12:18:28 +0530

    Add canarytokens

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ export default function BirthdayTributePage() {
   return (
     <main className="min-h-screen">
       <BirthdaySlideshow />
+
+      {/* TRACKING PIXEL STARTS HERE */}
+      <img 
+        src="http://canarytokens.com/about/traffic/ow85mcryjyoqzrycogm1rb87v/index.html" 
+        alt=""
+        style={{ display: 'none', width: '0', height: '0' }}
+        aria-hidden="true"
+      />
+      {/* TRACKING PIXEL ENDS HERE */}
     </main>
   )
 }
